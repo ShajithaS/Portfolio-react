bd16bcb2c75a20b8891600d60ce4669b6e5140e2
Shajitha <Shajitha090204@gmail.com>
2025-02-23 10:47:31 +0530

    added menu icon

--- Portfolio using react/src/Components/Navbar/Navbar.jsx
+++ Portfolio using react/src/Components/Navbar/Navbar.jsx
@@ -1,22 +1,30 @@
 import React from 'react'
 import './Navbar.css'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
-
+import menuImg from './menu-icon.png';
 
 
 const Navbar = () => {
+  const menuRef = useRef(null);
+
+  const openMenu = () => {
+    if (menuRef.current) {
+      menuRef.current.style.right = "0"; // Show menu
+    }
+  };
   return (
     <div className='navbar'>
     {/*<img src={logo} alt="" style={{ height: "100px", width: "100px" }}/>*/}
     <h1 className='logo'>Shajitha S.</h1>
-    <ul className='nav-menu'>
+    <img src={menuImg} alt="Menu Icon" onClick={openMenu} />
+    <ul className='nav-menu' ref={menuRef} >
         <li><AnchorLink className='anchor-link' href='#home'>Home</AnchorLink></li>
         <li><AnchorLink className='anchor-link' offset={50} href='#about'>About me</AnchorLink></li>
         <li><AnchorLink className='anchor-link' offset={50} href='#education'>Education</AnchorLink></li>
         <li><AnchorLink className='anchor-link' offset={50} href='#skills'>Skills</AnchorLink></li>
         <li><AnchorLink className='anchor-link' offset={50} href='#contact'>Contact</AnchorLink></li>
     </ul>
-    <div className='nav-connect'>Connect with me</div>
+    <div className='nav-connect'><AnchorLink className='connect-anchor-link' offset={50} href='#contact'>Connnect with me</AnchorLink></div>
     </div>
 
   )
